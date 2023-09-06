18884d22127767a4217512722b2b0c68aa1b6fb2
adis <vunicadis@gmail.com>
2023-09-06 20:59:44 +0200

    F, need css

--- script.js
+++ script.js
@@ -1,36 +1,76 @@
 const board = (function(){
+
     const cell=document.querySelectorAll('.cell')
-gameBoard = ["x","o","x","o","x","o","x","o","x"];
+    gameBoard = ["","","","","","","","",""];
+    let current="x"
+    const cong=document.querySelector('.cong')
+    const h1=document.querySelector('.h1')
+    const state=document.querySelector('.cong')
+    const h11=document.querySelector('.h11')
 
-render= function(){
+mark = function(){
+    for(let i=0; i<9; i++){
+        cell[i].addEventListener("click",function(){
+            if(!cell[i].innerHTML){
+                cell[i].innerHTML=current
+                gameBoard[i]=current
+                current=(current==="x")?"o":"x"
+                stateof()
+                check()
+                
+    }});
+}}
 
+check = function(){
+    for(let i=0;i<9;i++){
+        if(gameBoard[0]=="x" && gameBoard[4]=="x" && gameBoard[8]=="x"){congratulate("Player X "); ;break;}
+        if(gameBoard[2]=="x" && gameBoard[4]=="x" && gameBoard[6]=="x"){congratulate("Player X "); ;break}
+        if(gameBoard[0]=="x" && gameBoard[3]=="x" && gameBoard[6]=="x"){congratulate("Player X "); ;break}
+        if(gameBoard[1]=="x" && gameBoard[4]=="x" && gameBoard[7]=="x"){congratulate("Player X "); ;break}
+        if(gameBoard[2]=="x" && gameBoard[5]=="x" && gameBoard[8]=="x"){congratulate("Player X "); ;break}
+        if(gameBoard[0]=="x" && gameBoard[1]=="x" && gameBoard[2]=="x"){congratulate("Player X "); ;break}
+        if(gameBoard[3]=="x" && gameBoard[4]=="x" && gameBoard[5]=="x"){congratulate("Player X "); ;break}
+        if(gameBoard[6]=="x" && gameBoard[7]=="x" && gameBoard[8]=="x"){congratulate("Player X "); ;break}
 
-for(let i=0; i<9; i++){
- cell[i].innerHTML=gameBoard[i]
+        if(gameBoard[0]=="o" && gameBoard[4]=="o" && gameBoard[8]=="o"){congratulate("Player O "); ;break}
+        if(gameBoard[2]=="o" && gameBoard[4]=="o" && gameBoard[6]=="o"){congratulate("Player O "); ;break}
+        if(gameBoard[0]=="o" && gameBoard[3]=="o" && gameBoard[6]=="o"){congratulate("Player O "); ;break}
+        if(gameBoard[1]=="o" && gameBoard[4]=="o" && gameBoard[7]=="o"){congratulate("Player O "); ;break}
+        if(gameBoard[2]=="o" && gameBoard[5]=="o" && gameBoard[8]=="o"){congratulate("Player O "); ;break}
+        if(gameBoard[0]=="o" && gameBoard[1]=="o" && gameBoard[2]=="o"){congratulate("Player O "); ;break}
+        if(gameBoard[3]=="o" && gameBoard[4]=="o" && gameBoard[5]=="o"){congratulate("Player O "); ;break}
+        if(gameBoard[6]=="o" && gameBoard[7]=="o" && gameBoard[8]=="o"){congratulate("Player O "); ;break}
+    }
 }
+reset = function(){
+    for(let i=0;i<9;i++){
+        cell[i].innerHTML=""
+        gameBoard[i]=""
+        current="x"
+        cong.style.display="none"
+        h11.innerHTML="Player x's turn"
+    }
 }
+
+res=document.querySelector('.reset').addEventListener("click", ()=>{
+    reset()
+})
+
+   congratulate = function(name){
+    
+    h11.innerHTML=`${name}wins`
+   }
+   stateof = function(){
+    
+    h11.innerHTML=(current=="x")?"Player X's turn":"Player O's turn"
+   } 
+
 
 return{
-    render
+    mark,
 }
 
 
 })()
-board.render()
 
-
-
-
-
-const players =()=>{
-
- const player1 = {
-
-    }
- const player2 = {
-
-    }
-return{
-    player1,player2
-}
-}
+board.mark()
